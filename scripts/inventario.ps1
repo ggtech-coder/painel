be86510f4933254d8603f3e6dce3e22a178cee6a
ggtech-coder <gg.tech.suporte@gmail.com>
2026-05-21 16:44:59 -0300

    Create inventario.ps1

---
+++ scripts/inventario.ps1
@@ -0,0 +1,16 @@
+# INVENTÁRIO AUTOMÁTICO WINDOWS
+
+$pc = $env:COMPUTERNAME
+$user = $env:USERNAME
+
+$cpu = (Get-WmiObject Win32_Processor).Name
+$ram = [math]::Round((Get-WmiObject Win32_ComputerSystem).TotalPhysicalMemory /1GB)
+
+$data = @{
+    computador = $pc
+    usuario = $user
+    cpu = $cpu
+    ram = "$ram GB"
+}
+
+$data | ConvertTo-Json
